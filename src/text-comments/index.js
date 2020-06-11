04bd0d1bb490870c936fb8c421ce6242b5a5db8a
Nathan Smith <nathan.smith12@nhs.net>
2020-06-11 13:46:08 +0100

    Remove trailing spaces

--- src/text-comments/index.js
+++ src/text-comments/index.js
@@ -29,11 +29,11 @@ export default class TextCommentsScreen extends Screen {
     const noLabel = 'What were you looking for?';
     const label = this.getInitialQuestionResponse() ? yesLabel : noLabel;
     const html = template.replace('{{ label }}', label);
-
+    
     const node = this.updateHtml(html);
     const moreDetail = node.querySelector('#more-detail');
     moreDetail.focus();
-    
+
     this.addListeners(node);
   }
 }
