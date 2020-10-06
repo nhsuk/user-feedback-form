de259c7ed1b928ac7665c258dcbefd6abadf43f8
Michael Cheung <michaelcheung@nhs.net>
2020-10-06 17:25:55 +0100

    Fix linting error

--- tests/integration/jest.config.js
+++ tests/integration/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   displayName: 'Integration tests',
   preset: 'jest-puppeteer',
   rootDir: '.',
-  testTimeout: 10000,
   testMatch: [
     '<rootDir>/*.test.js',
   ],
+  testTimeout: 10000,
 };
